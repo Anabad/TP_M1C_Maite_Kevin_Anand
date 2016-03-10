f4ef6169a233b1c98a1045556596792b39b2c6e3
DidelotK <Didelot_k@esme.fr>
2016-03-10 23:25:06 +0100

    MISE a jour
    Changement réaliser sur la partie ajouter

--- TP_Python/Annuaire_Anand/View.py
+++ TP_Python/Annuaire_Anand/View.py
@@ -28,10 +28,10 @@ class View(QMainWindow):
 		self.barHaut = QWidget(self)
 		self.barHaut.setStyleSheet("background:url(IMAGES/texture.jpg);border-bottom:3px groove black")
 		
-		self.dessousbar =QWidget(self)
+		self.dessousBar =QWidget(self)
 		
 		self.layoutConteneur.addWidget(self.barHaut,0,0,1,1)
-		self.layoutConteneur.addWidget(self.dessousbar,1,0,6,1)
+		self.layoutConteneur.addWidget(self.dessousBar,1,0,6,1)
 		self.conteneur.setLayout(self.layoutConteneur)
 		
 		# Définition des widgets dans la bar du haut
@@ -55,13 +55,17 @@ class View(QMainWindow):
 		# Définition des widgets dans la partie inférieur de l'écran
 		self.layoutDessousBar = QGridLayout()
 		
-		self.barGauche = QWidget(self.dessousbar)
+		self.barGauche = QWidget(self.dessousBar)
 		
-		self.zoneAffichage = QScrollArea(self.dessousbar)
+		self.zoneAddMod = QWidget(self.dessousBar)
+		self.zoneAffichage = QScrollArea()
 		
 		self.layoutDessousBar.addWidget(self.barGauche,0,0,1,1)
+		self.layoutDessousBar.addWidget(self.zoneAddMod,0,1,1,5)
 		self.layoutDessousBar.addWidget(self.zoneAffichage,0,1,1,5)
-		self.dessousbar.setLayout(self.layoutDessousBar)
+		self.zoneAddMod.hide()
+		
+		self.dessousBar.setLayout(self.layoutDessousBar)
 		
 		# Définition des widgets dans la barre de gauche
 		self.layoutBarGauche = QVBoxLayout()
@@ -74,12 +78,62 @@ class View(QMainWindow):
 		self.layoutBarGauche.addWidget(self.boutonEditer)
 		self.layoutBarGauche.addWidget(self.boutonSupprimer)
 		self.barGauche.setLayout(self.layoutBarGauche)
+		
+		# Définition du widget conteneur du ScrollArea
+		self.conteneurScrollArea = QWidget(self.zoneAffichage)
+		
+		# Définition du widgets qui va contenir le formulaire pour ajouter et modifier un contact
+		
+		self.layoutConteneurAddMod = QGridLayout()
+		
+		self.titreLabel = QLabel("*******", self)
+		self.nomAddModLabel = QLabel("NOM", self)
+		self.prenomAddModLabel = QLabel("PRENOM", self)
+		self.numeroAddModLabel = QLabel("NUMERO", self)
+		self.adresseAddModLabel = QLabel("ADRESSE", self)
+		self.typeAddModLabel = QLabel("TYPE", self)
+		self.nomAddModWidget = QLineEdit(self.conteneurScrollArea)
+		self.prenomAddModWidget = QLineEdit("prenom",self.conteneurScrollArea)
+		self.numeroAddModWidget = QLineEdit(self.conteneurScrollArea)
+		self.adresseAddModWidget = QLineEdit(self.conteneurScrollArea)
+		self.typeAddModWidget = QLineEdit(self.conteneurScrollArea)
+		self.boutonValiderAddMod = QPushButton("Valider",self.conteneurScrollArea)
+		
+		
+		
+		self.nomAddModWidget.setStyleSheet("background-color:rgb(255,255,255)")
+		self.prenomAddModWidget.setStyleSheet("background-color:rgb(255,255,255)")
+		self.numeroAddModWidget.setStyleSheet("background-color:rgb(255,255,255)")
+		self.adresseAddModWidget.setStyleSheet("background-color:rgb(255,255,255)")
+		self.typeAddModWidget.setStyleSheet("background-color:rgb(255,255,255)")
+		
+		self.layoutConteneurAddMod.addWidget(self.titreLabel,0,3,1,1)
+		self.layoutConteneurAddMod.addWidget(self.nomAddModLabel,1,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.nomAddModWidget,2,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.prenomAddModLabel,3,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.prenomAddModWidget,4,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.numeroAddModLabel,5,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.numeroAddModWidget,6,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.adresseAddModLabel,7,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.adresseAddModWidget,8,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.typeAddModLabel,9,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.typeAddModWidget,10,0,1,7)
+		self.layoutConteneurAddMod.addWidget(self.boutonValiderAddMod,11,3,1,1)
+		self.zoneAddMod.setLayout(self.layoutConteneurAddMod)
+		
 
 	def connecterWidget(self):
-		self.connect(self.boutonAjouter, SIGNAL("clicked()"),SLOT("SLOT_Ajouter()"))
+		self.connect(self.boutonAjouter, SIGNAL("clicked()"),self.SLOT_Ajouter)
 			
 	def SLOT_Ajouter(self):
-		print("PATROOOON")
+		self.zoneAffichage.hide()
+		self.titreLabel.setText("Ajouter")
+		self.nomAddModWidget.setText("")
+		self.prenomAddModWidget.setText("")
+		self.numeroAddModWidget.setText("")
+		self.adresseAddModWidget.setText("")
+		self.typeAddModWidget.setText("")
+		self.zoneAddMod.show()
 		
 		
 		
