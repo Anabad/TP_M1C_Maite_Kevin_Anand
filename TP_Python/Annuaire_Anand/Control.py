a4aaaf93591ea8e907584db5b60523d5aa77fb66
DidelotK <Didelot_k@esme.fr>
2016-03-11 17:52:37 +0100

    Modification pour base de donnée 1

--- TP_Python/Annuaire_Anand/Control.py
+++ TP_Python/Annuaire_Anand/Control.py
@@ -5,4 +5,18 @@
 
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+from Model import Model
+from View import View
 import sys
+
+class Control:
+	def __init__(self):
+		self.view
+		self.model
+	def controlerEnvoyer(self,dictionnaire):
+		ajouter_contact(dictionnaire)
+	def setView(self,view):
+		self.view=view
+	def setModel(self,model):
+		self.model=model
+	
